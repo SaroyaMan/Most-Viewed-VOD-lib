8efa3eb454075c28f12906c651e99a7344565c4a
Yoav Saroya <stankovic100@gmail.com>
2017-05-12 19:36:38 +0300

    exercise1.1 release

--- Version 1.0/WS-Most-Watched-VOD-lib/most_watched/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    //Json error in case of bad use
-    jsonError: {"error": "Video(s) is not found"},
-
-    //Error descriptions in case of video has not been found
-    idNotExistsDesc : "ID is not exists",
-    limitExceptionDesc: "No results for videos with this limit of views",
-    localExceptionDesc: "No results for videos with that language"
-};
